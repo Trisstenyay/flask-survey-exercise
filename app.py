2ab8e9f0c93588f50b1b79f3b97e30b537312ae2
Tristan Tenyay <trisst3nyay@gmail.com>
2024-10-15 14:15:17 -0400

    added all files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 
 from surveys import satisfaction_survey
 
@@ -7,7 +7,7 @@ app.secret_key = 'flash_messages'  # You can use any string
 
 
 
-responses = []
+
 
 @app.route('/')
 def start_page():
@@ -17,38 +17,50 @@ def start_page():
 
 
 
+@app.route('/middleware', methods=["POST"])
+def middleware():
+    if request.method == 'POST':
+        if 'responses' not in session:
+            session['responses'] = []
+    return redirect(url_for('question', question_number=len(session['responses'])))
+    
+
+
 
 @app.route('/questions/<int:question_number>', methods= ["GET", "POST"])
 def question(question_number):
+    print('question', len(session['responses']))
+    print('question2', question_number)
     # Check if the user has answered all questions
-    if len(responses) == len(satisfaction_survey.questions):
+    if len(session['responses']) == len(satisfaction_survey.questions):
         return redirect(url_for('thank_you'))
     
-    # Check if the question number in the URL is out of range
+    # # Check if the question number in the URL is out of range
     if question_number >= len(satisfaction_survey.questions):
-        # Redirect to the correct question if it doesn't exist
+    #     # Redirect to the correct question if it doesn't exist
         flash("Invalid question number!")
-        return redirect(url_for('question', question_number=len(responses)))
+        return redirect(url_for('question', question_number=len(session['responses'])))
     
-     # Check if the user is trying to access a question they haven't reached yet
-    if question_number != len(responses):
-        return redirect(url_for('question', question_number=len(responses)))
+    #  # Check if the user is trying to access a question they haven't reached yet
+    if question_number != len(session['responses']):
+        return redirect(url_for('question', question_number=len(session['responses'])))
     
     question = satisfaction_survey.questions[question_number]
     return render_template('questions.html', question=question, question_number=question_number)
     
 
 
-@app.route('/answer', methods= ["GET", "POST"])
+@app.route('/answer', methods= ["POST"])
 def answer():
     if request.method == "POST":
         value = request.form.get('answer')
-        responses.append(value)
-    if len(responses) == len(satisfaction_survey.questions):
-        return redirect('/thankyou')
-    else:
-         return redirect(f'/questions/{len(responses)}')
-    return responses
+        
+        if 'responses' in session:
+            responses = session['responses']
+            responses.append(value)
+            session['responses'] = responses
+    return redirect(f'/questions/{len(session["responses"])}')
+
 
     
 
